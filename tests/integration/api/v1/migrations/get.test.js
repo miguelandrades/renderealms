70bf2942c68dca49cc26cd0c78ef1fd22794f2e4
Miguel Andrade <72988775+miguelandrades@users.noreply.github.com>
2025-09-10 19:23:39 +0000

    Estabilizando testes utilizando `async-retry` e `orchestrator.js`

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,10 +1,10 @@
 import database from "infra/database.js";
+import orchestrator from "tests/orchestrator.js";
 
-beforeAll(cleanDatabase);
-
-async function cleanDatabase() {
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
   await database.query("TRUNCATE TABLE pgmigrations RESTART IDENTITY CASCADE");
-}
+});
 
 test("GET to api/v1/migrations Should Return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/migrations");
---
+++ tests/orchestrator.js
@@ -0,0 +1,31 @@
+import retry from "async-retry";
+
+const waitForAllServices = async () => {
+  const fetchStatusPage = async () => {
+    const response = await fetch("http://localhost:3000/api/v1/status");
+
+    if (!response.ok) {
+      throw new error(`Servidor não disponível: ${response.status}`);
+    }
+
+    let responseBody;
+    try {
+      responseBody = await response.json();
+    } catch (error) {
+      throw new error("Respposta Não é um JSON Válido");
+    }
+  };
+
+  const waitForWebServer = async () => {
+    return retry(fetchStatusPage, {
+      retries: 100,
+      maxTimeout: 60000,
+    });
+  };
+
+  await waitForWebServer();
+};
+
+export default {
+  waitForAllServices,
+};
