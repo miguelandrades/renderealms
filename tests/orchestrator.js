ac2ffe994e44801fe1a62df920d9fad0cf782505
Miguel Andrade <nitrusbr@gmail.com>
2025-09-11 15:46:46 -0300

    Ajustando  para bater no migrations, e ajustando npm test para windowns

--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -2,7 +2,7 @@ import retry from "async-retry";
 
 const waitForAllServices = async () => {
   const fetchStatusPage = async () => {
-    const response = await fetch("http://localhost:3000/api/v1/status");
+    const response = await fetch("http://localhost:3000/api/v1/migrations");
 
     if (!response.ok) {
       throw new error(`Servidor não disponível: ${response.status}`);
@@ -19,7 +19,7 @@ const waitForAllServices = async () => {
   const waitForWebServer = async () => {
     return retry(fetchStatusPage, {
       retries: 100,
-      maxTimeout: 60000,
+      maxTimeout: 1000,
     });
   };
 
